b292e9d91fe7eb01e9ebf33468fb197648c46a97
shaurya0806 <53405801+shaurya0806@users.noreply.github.com>
2022-07-10 18:27:12 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import About from "./About";
+import './App.css';
+import Home from './Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <About />
+    </div>
+  );
+}
+
+export default App;
